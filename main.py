60cce28a177480abbf57ee72ea6bce9c6882f6ca
Ervīns Jakovelis <125136553+ErvinsJakovelis@users.noreply.github.com>
2023-02-13 15:42:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,31 +8,31 @@ Bracket = namedtuple("Bracket", ["char", "position"])
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
+opening_brackets_stack = []
 
 def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
+    global opening_brackets_stack
+    for  i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i+1))
-
         if next in ")]}":
-            if(len(opening_brackets_stack) == 0 or are_matching(opening_brackets_stack[-1][0], next ) == False):
+            if(len(opening_brackets_stack) == 0 or are_matching(opening_brackets_stack[-1][0], next) == False):
                 print(i+1)
-                return False
-            else:
-                return("Success")
+                return 0
+            opening_brackets_stack.pop()
 
 
+        
 
 def main():
-#     ievade = input()
-#     if ievade == "I":
-
-       text = input()
-       mismatch = find_mismatch(text)
-       if (mismatch == "Success"):
-           print("Success")
-        
+    text = input()
+    mismatch = find_mismatch(text)
+    if(len(opening_brackets_stack) != 0 ):
+        print(opening_brackets_stack[-1][1])
+    elif mismatch == 0:
+        pass
+    else:
+        print("Success")
 
 if __name__ == "__main__":
     main()
