4b4e42613e05450389347152627abeafb6f15aa2
Ervīns Jakovelis <125136553+ErvinsJakovelis@users.noreply.github.com>
2023-02-19 19:26:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-#python 3
-
 from collections import namedtuple
 # from tkinter.filedialog import askopenfilename
 Bracket = namedtuple("Bracket", ["char", "position"])
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
-opening_brackets_stack = []
+
 
 def find_mismatch(text):
-    opening_brackets_stack
+    opening_brackets_stack = []
     for  i, next in enumerate(text):
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i+1))
