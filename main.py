02daa0ef5fe0604036d2994d4db7762c221b445f
Ervīns Jakovelis <125136553+ErvinsJakovelis@users.noreply.github.com>
2023-02-19 19:37:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 from collections import namedtuple
-# from tkinter.filedialog import askopenfilename
 Bracket = namedtuple("Bracket", ["char", "position"])
 def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
-
-
 def find_mismatch(text):
     opening_brackets_stack = []
     for  i, next in enumerate(text):
+
         if next in "([{":
             opening_brackets_stack.append(Bracket(next, i+1))
+    
         if next in ")]}":
             if(len(opening_brackets_stack) == 0 or are_matching(opening_brackets_stack[-1][0], next) == False):
                 print(i+1)
                 return 0
             opening_brackets_stack.pop()
+
     return opening_brackets_stack
+    
 def main():
     ievade = input()
     if 'I' in ievade:
         text = input()
+    elif 'F' in ievade:
+        print("Ievadiet ceļu līdz teksta failam: ")
+        ievade = input()
+        text = open(ievade, "r").readline()
     mismatch = find_mismatch(text)
     if mismatch == 0:
         pass
